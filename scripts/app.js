17d598ca7cade4708c34752c13e000c26f0548e1
wallandall <wallandall@gmail.com>
2019-04-22 14:26:58 +0200

    Addedimages, icons and styling

--- scripts/app.js
+++ scripts/app.js
@@ -1,20 +1,35 @@
 const cityForm = document.querySelector('.change-location');
 const card = document.querySelector('.card');
 const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
 
 const updateUI = (data) => {
-    const cityInfo = data.cityInfo;
-    const weather = data.weather;
-    
+    const {cityInfo, weather} = data;
+    console.log(cityInfo);
+    console.log(weather);
 
     details.innerHTML = `
-        <h5 class="my-3">${cityInfo.EnglishName}</h5>
-        <div class="my-3">${weather.WeatherText}</div>
-        <div class="display-4 my-4">
-        <span>${weather.Temperature.Metric.Value}</span>
-        <span>&deg;C</span>
-        </div>
-    `;
+    <h5 class="my-3">${cityInfo.EnglishName}</h5>
+    <div class="my-3">${weather.WeatherText}</div>
+    <div class="display-4 my-4">
+      <span>${weather.Temperature.Metric.Value}</span>
+      <span>&deg;C</span>
+    </div>
+  `;
+
+
+    const iconSrc = `images/icons/${weather.WeatherIcon}.svg`;
+    icon.setAttribute('src', iconSrc);
+
+    let timeSrc = null;
+    if(weather.IsDayTime){
+        timeSrc = 'images/day.svg';
+    } else {
+        timeSrc = 'images/night.svg';
+    }
+
+    time.setAttribute('src', timeSrc);
 
     if(card.classList.contains('d-none')){
         card.classList.remove('d-none');
